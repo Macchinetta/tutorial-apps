8a40adcb09bbd00f2d5825730697a1fe513ab68a
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2023-04-24 18:19:24 +0900

    Release 1.9.1.RELEASE

--- secure-login-demo/secure-login-thymeleaf/secure-login-thymeleaf-web/src/main/java/com/example/securelogin/app/common/error/CommonErrorController.java
+++ secure-login-demo/secure-login-thymeleaf/secure-login-thymeleaf-web/src/main/java/com/example/securelogin/app/common/error/CommonErrorController.java
@@ -2,6 +2,7 @@ package com.example.securelogin.app.common.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
